aafe236123f79e99291ec64bf332b09a42222953
bill-burgess <wmhburgess@gmail.com>
2017-08-24 15:37:44 +1200

    WalkRequest placeholder made

--- client/components/App.js
+++ client/components/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import Navbar from './Navbar'
-import Quote from './Quote'
-
+import WalkRequest from './WalkRequest'
+import { HashRouter as Router, Route } from 'react-router-dom'
 const App = () => {
   return (
     <div>
@@ -9,7 +9,7 @@ const App = () => {
 
       <Navbar />
       <div className='quote'>
-        <Quote />
+        <Route exact path='/walkrequest' component={WalkRequest}>Login</Route>
       </div>
     </div>
   )
