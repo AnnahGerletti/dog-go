1d50b9bd1ef18b2bc509beca30c1b7431d49f8d1
eda013 <poukupenga@gmail.com>
2017-08-25 18:59:25 +1200

    test to add walker in reducer passes

--- tests/client/reducers.test.js
+++ tests/client/reducers.test.js
@@ -3,8 +3,20 @@ import '../setup-dom'
 // var register = require('../../client/reducers/register')
 import register from '../../client/reducers/register'
 
-test.only('initial state empty', t => {
+test('initial state empty', t => {
   const expected = []
-  // const actual = register(undefined, {})
-  // t.deepEqual(actual, expected)
+  const actual = register(undefined, {})
+  t.deepEqual(actual, expected)
+})
+
+test('state adds a walker', t => {
+  const state = []
+  const action = {
+    type: 'ADD_WALKER',
+    walker: {name: 'timmy'}
+  }
+  const expected = [{name: 'timmy'}]
+  const actual = register(state, action)
+
+  t.deepEqual(actual, expected)
 })
