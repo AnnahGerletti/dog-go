2752ffe0a118ff517e4be438e34007141d1d9cb2
eda013 <poukupenga@gmail.com>
2017-08-25 17:41:18 +1200

    client tests setup, state passing

--- client/reducers/register.js
+++ client/reducers/register.js
@@ -1,10 +1,10 @@
-export default function register(state = [], action) {
+export default function register (state = null, action) {
   switch (action.type) {
     case 'ADD_WALKER':
       return [...state, action.walker]
     case 'ADD_OWNER':
       return [...state, action.owner]
-  default:
-    return state
+    default:
+      return state
   }
 }
---
+++ tests/client/reducers.test.js
@@ -0,0 +1,10 @@
+import test from 'ava'
+import '../setup-dom'
+// var register = require('../../client/reducers/register')
+import register from '../../client/reducers/register'
+
+test.only('initial state empty', t => {
+  const expected = []
+  // const actual = register(undefined, {})
+  // t.deepEqual(actual, expected)
+})
