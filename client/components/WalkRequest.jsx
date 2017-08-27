c8aa0b0c26ef4be6920fc8838c0937b7ee31fe16
eda013 <poukupenga@gmail.com>
2017-08-28 11:14:48 +1200

    button shows a walk, not sending walkReq yet

--- client/components/WalkRequest.jsx
+++ client/components/WalkRequest.jsx
@@ -5,19 +5,36 @@ import {Link} from 'react-router-dom'
 import {receiveOwnersRequest} from '../actions/receiveOwners'
 
 class WalkRequest extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      showWalk: false
+    }
+  }
+
+  toggleWalk () {
+    this.setState({
+      showWalk: !this.state.showWalk
+    })
+  }
 
   componentDidMount() {
     this.props.dispatch(receiveOwnersRequest())
   }
   render() {
-    const{receive}=this.props
+    const {receive} = this.props
+    const {showWalk} = this.state
     return(
       <div className='container'>
         <h2 className='subHeader'>Dog List</h2>
-        <ul>
-            {receive.map((owner, i) => {
-              return <li key={i}>the owners are -- {owner.owner_name} their dogs are called {owner.dog_name}</li>})}
-        </ul>
+        <div className="dogList">
+          <ul>
+              {receive.map((owner, i) => {
+                return <li key={i}>the owners are -- {owner.owner_name} their dogs are called <button className="dogWalk loneButton" onClick={this.toggleWalk.bind(this)}>{owner.dog_name}</button>
+                </li>})}
+                {showWalk && <button className='requestWalk loneButton'>Walk</button>}
+          </ul>
+      </div>
       </div>
     )
   }
