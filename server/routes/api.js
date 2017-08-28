6cfecf9268044de1575fba531056e25465fc1326
Your Name <you@example.com>
2017-08-29 10:27:33 +1200

    fixing register route

--- server/routes/api.js
+++ server/routes/api.js
@@ -9,26 +9,29 @@ router.use(bodyParser.json())
 
 // This is the only API route that uses local strategy,
 // to check if we can issue a JWT in response to requests.
-router.post('/authenticate', auth.issueJwt)
-
+// router.post('/authenticate', auth.issueJwt)
+//
 router.post('/register',
   register,
   auth.issueJwt
 )
 
 function register (req, res, next) {
-  users.exists(req.body.username)
+  console.log('hi')
+  users.exists(req.body.username, req.app.get('db'))
     .then(exists => {
       if (exists) {
         return res.status(400).send({ message: 'User exists' })
       }
       // req.login() can be used to automatically log the user in after registering
-      users.create(req.body.username, req.body.password)
+      users.create(req.body.username, req.body.password, req.app.get('db'))
         .then(() => next())
     })
     .catch(err => {
       res.status(400).send({ message: err.message })
     })
 }
+
+//TODO: login route
 
 module.exports = router
