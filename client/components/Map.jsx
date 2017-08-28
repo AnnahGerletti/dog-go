13ea24414728d49e451c22ed2279bde96e37ee04
Your Name <you@example.com>
2017-08-28 12:04:30 +1200

    map and marker works

--- client/components/Map.jsx
+++ client/components/Map.jsx
@@ -1,33 +1,36 @@
 import React from 'react'
 import {Map, Marker, GoogleApiWrapper} from 'google-maps-react';
 
-
 class MapContainer extends React.Component {
-  constructor (props) {
+  constructor(props) {
     super(props)
     console.log(props.google)
   }
-
   render() {
     return (
-      <Map google={this.props.google}
-        style={{width: '95%', height: '85%', position: 'relative', margin: '10px'}}
-        className={'map'}
-        zoom={6}
-        initialCenter={{
-            lat: -36.8547512,
-            lng: 174.7787425
-          }}
-          >
-    </Map>
+      <div>
+        <Map google={this.props.google} style={{
+          width: '45%',
+          height: '45%',
+          position: 'relative',
+          margin: '0 auto'
+        }} className={'map'} zoom={13} fullscreenControl={true} containerStyle={{}} initialCenter={{
+          lat: -41.2865,
+          lng: 174.7762
+        }}>
+
+          <Marker title={'place.place'} name={'place.place'} position={{
+            lat: -41.2865,
+            lng: 174.7762
+          }}/>
+        </Map>
+      </div>
     );
   }
 }
 
-
-
 export default GoogleApiWrapper({
-  apiKey: ("AIzaSyBtks1ielOp7wqVy`IJNevVW-8SrmpSf8Pk")
+  apiKey: "AIzaSyBtks1ielOp7wqVyIJNevVW-8SrmpSf8Pk"
 })(MapContainer)
 
 // https://github.com/fullstackreact/google-maps-react/blob/master/README.md
