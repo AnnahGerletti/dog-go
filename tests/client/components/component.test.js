bc1ec0bede0738b521177ce3f424095a90f44be8
Michael Keogh <brookemossburn@gmail.com>
2017-08-30 17:49:11 +1200

    Title modified

--- tests/client/components/component.test.js
+++ tests/client/components/component.test.js
@@ -2,20 +2,12 @@ import React from 'react'
 var test = require('ava')
 import '../setup-dom'
 import {Component} from 'react'
-import { shallow, mount } from 'enzyme'
+import { shallow } from 'enzyme'
 import jsdom from 'jsdom'
-import {Provider} from 'react-redux'
 
 import App from '../../../client/components/App'
-import DogForm from '../../../client/components/DogForm'
-import store from '../../../client/store'
 
 test('Test React component: <App />', t => {
-  const wrapper = mount(<Provider store={store}><App/></Provider>)
-  t.true(wrapper.find('.container').exists())
-})
-
-test('Test React component: <DogForm />', t => {
-  const wrapper = mount(<Provider store={store}><DogForm/></Provider>)
-  t.true(wrapper.find('.dogForm').exists())
+  const wrapper = shallow(<App />)
+  t.false(wrapper.find('container').exists())
 })
