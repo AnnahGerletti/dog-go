4665e534a89a77eab00cbc98ce19470defeb3cde
AnnahGerletti <agerletti@gmail.com>
2017-09-10 16:17:39 +1200

    fixing spelling errors

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,15 +1,12 @@
 import React from 'react'
 import {connect} from 'react-redux'
-import {Link} from 'react-router-dom'
-
 
 import Logout from './Logout'
-
 
 const Home = (props) => {
   return (
     <div className="HomeCon">
-  Dog-Go is a brand new service, which connects people who love to walk dogs  and with people who can't always  find the time to walk their dogs.<br/><br/><strong>We know there is nothing more a dog loves then to be walked. </strong>
+  Dog-Go is a brand new service, which connects people who have a love of walking dogs, with people who can't always find the time to walk their dogs.<br/><br/><strong>We know there is nothing more a dog loves then to be walked. </strong>
     </div>
   )
 }
